bc81f927aaef9400c82b9890705a269916da2527
balancenutri <support@balancenutrition.com>
2024-08-17 18:27:27 +0530

    added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { connectToDb } from "./src/db/db.js";
-import {homeRouter, loginRouter, userRouter} from "./src/routes/user.route.js";
+import {
+  homeRouter,
+  loginRouter,
+  userRouter,
+} from "./src/routes/user.route.js";
 import { jwtVerifyToken } from "./src/middlewares/authJwt.js";
 import cookieParser from "cookie-parser";
 const port = process.env.PORT || 5000;
@@ -10,9 +14,12 @@ app.use(cookieParser());
 app.use(express.json());
 connectToDb();
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1" ,homeRouter);
-app.use("/api/v1/login" ,loginRouter);
+app.use("/api/v1", homeRouter);
+app.use("/api/v1/login", loginRouter);
 
+app.get("/", (req, res) => {
+  res.send("asdfghj");
+});
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
